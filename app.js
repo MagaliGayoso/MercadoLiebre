67954b5e23163b67e9904ba824a93ad215a9c4b0
magaligayoso <maga.casla.gayoso@gmail.com>
2022-08-01 17:01:09 -0300

    register

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ app.use(express.static('public'));
 
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, 'views', 'home.html')));
 
+app.get("/register", (req, res) => res.sendFile(path.join(__dirname, 'views', 'register.html')));
+
+
 app.listen(port, () =>
 {
     console.log("Servidor corriendo (http://localhost:" + port + "/)");
