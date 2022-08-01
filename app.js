c5b1fded7bb117f7f5b785e7d5e3245c903c9890
magaligayoso <maga.casla.gayoso@gmail.com>
2022-08-01 16:11:00 -0300

    login terminado

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ app.use(express.static('public'));
 
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, 'views', 'home.html')));
 
+app.get("/login", (req, res) => res.sendFile(path.join(__dirname, 'views', 'login.html')));
+
+
 app.listen(port, () =>
 {
     console.log("Servidor corriendo (http://localhost:" + port + "/)");
